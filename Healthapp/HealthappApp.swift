84a7a4672c8469158cd08cbeb8c7bd8cd258d7f9
Ricky Elder <ricky.elder07@gmail.com>
2025-12-25 16:17:39 -0800

    Initial Commit

---
+++ Healthapp/HealthappApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthappApp.swift
+//  Healthapp
+//
+//  Created by Ricky Elder on 12/25/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
